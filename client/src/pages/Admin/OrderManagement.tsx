5fbeec40b729fbc85ab56e96255dcdc1dd681ca3
kauntak <n.koshirae+github@gmail.com>
2022-08-01 16:56:05 -0600

    Adding Supplier

--- client/src/pages/Admin/OrderManagement.tsx
+++ client/src/pages/Admin/OrderManagement.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import styles from "./../../css/management.module.css";
 
 import { BsPencil, BsPlusLg, BsTrash } from "react-icons/bs";
-import { createOrderCategory, createOrderItem, deleteOrderCategory, getOrderCategories, getOrderItems, getSuppliers, updateOrderCategory, updateOrderItem } from "../../api";
+import { createOrderCategory, createOrderItem, createSupplier, deleteOrderCategory, deleteOrderItem, deleteSupplier, getOrderCategories, getOrderItems, getSuppliers, updateOrderCategory, updateOrderItem, updateSupplier } from "../../api";
 import { AddAndEditComponent } from "../../components/AddAndEditComponent";
 import { ButtonComponent } from "../../components/ButtonComponent";
 import { DataTable } from "../../components/DataTable";
@@ -30,6 +30,7 @@ export const OrderManagement: React.FC<Props> = () => {
     const [itemList, setItemList] = useState<IOrderItem[]>([]);
     const [selectedItemId, setSelectedItemId] = useState<string>("");
     const [supplierList, setSupplierList] = useState<ISupplier[]>([]);
+    const [selectedSupplierId, setSelectedSupplierId] = useState<string>("");
 
     useEffect(() => {
         getOrderCategories()
@@ -181,6 +182,22 @@ export const OrderManagement: React.FC<Props> = () => {
                 fieldType:"Single",
                 required:false,
                 field:{value:""}
+            }, {
+                name:"Supplier",
+                display:"Supplier",
+                fieldType:"Multi",
+                required: false,
+                field: {
+                    isMultiSelect:false,
+                    name:"Supplier",
+                    list: supplierList.map(category => {
+                        return {
+                            id:category._id,
+                            name:category.name,
+                            display:category.name
+                        }
+                    })
+                }
             }
         ]
         setAddEditTitle("Add Item");
@@ -191,11 +208,17 @@ export const OrderManagement: React.FC<Props> = () => {
     const onEditItemClick = (e:React.MouseEvent<HTMLButtonElement>):void => {
         const itemToEdit = itemList.find(item => item._id === selectedItemId);
         if(itemToEdit === undefined) return;
-        const itemCategory = categoryList.find(category => category._id === itemToEdit._id);
-        const selected = itemCategory === undefined? undefined: {
+        const itemCategory = categoryList.find(category => category._id === itemToEdit.category);
+        const selectedCategory = itemCategory === undefined? undefined: {
             id: itemCategory._id,
             name: itemCategory.name,
             display: itemCategory.name
+        };
+        const itemSupplier = supplierList.find(supplier => supplier._id === itemToEdit.supplier)
+        const selectedSupplier = itemSupplier === undefined? undefined: {
+            id: itemSupplier._id,
+            name: itemSupplier.name,
+            display: itemSupplier.name
         };
         const editFields:Field[] = [
             {
@@ -218,7 +241,7 @@ export const OrderManagement: React.FC<Props> = () => {
                             display:category.name
                         }
                     }),
-                    selected
+                    selected: selectedCategory
                 },
 
             },{
@@ -227,6 +250,24 @@ export const OrderManagement: React.FC<Props> = () => {
                 fieldType:"Single",
                 required:false,
                 field:{value:itemToEdit.description}
+            }, {
+                name:"Supplier",
+                display:"Supplier",
+                fieldType:"Multi",
+                required: false,
+                field: {
+                    isMultiSelect:false,
+                    name:"Supplier",
+                    list: supplierList.map(category => {
+                        return {
+                            id:category._id,
+                            name:category.name,
+                            display:category.name
+                        }
+                    }),
+                    selected: selectedSupplier
+                },
+
             }
         ];
         setAddEditTitle("Edit " + itemToEdit.name);
@@ -235,7 +276,24 @@ export const OrderManagement: React.FC<Props> = () => {
     }
     
     const onDeleteItemClick = (e:React.MouseEvent<HTMLButtonElement>):void => {
+        const itemToDelete = itemList.find(item => item._id === selectedItemId);
+        if(itemToDelete === undefined) return;
+        setWarningMessage(text.warning.delete.replace("{replace}", itemToDelete.name));
+        setWarningButton(()=> onConfirmDeleteItemClick);
+        setCanCancel(true);
+        setShowWarning(true);
+    }
 
+    const onConfirmDeleteItemClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+        setWarningButton(() => defaultButton);
+        deleteOrderItem(selectedItemId).then(res => {
+            setItemList(res.orderItems);
+        }).catch(err => {
+            setWarningMessage(err);
+            setCanCancel(false);
+            setShowWarning(true);
+        });
+        setShowWarning(false);
     }
 
     const onSaveItemButton = (e:React.MouseEvent<HTMLButtonElement>, fields:Field[]):void => {
@@ -243,7 +301,8 @@ export const OrderManagement: React.FC<Props> = () => {
             _id: selectedItemId===""?undefined:selectedItemId,
             name: (fields[0]?.field as SingleField)?.value as string,
             category:(fields[1].field as MultiField).selected?.id!,
-            description:(fields[1]?.field as SingleField)?.value as string
+            description:(fields[2]?.field as SingleField)?.value as string,
+            supplier: (fields[3].field as MultiField).selected?.id!
         }
         if(selectedItemId===""){
             createOrderItem(newItem)
@@ -255,6 +314,95 @@ export const OrderManagement: React.FC<Props> = () => {
                 setItemList(res.orderItems);
             });
         setShowAddEditComponent(false);
+    }
+
+    const onSaveSupplierButton = (e:React.MouseEvent<HTMLButtonElement>, fields:Field[]):void => {
+        const newSupplier:ISupplier = {
+            _id: selectedItemId===""?undefined:selectedItemId,
+            name: (fields[0]?.field as SingleField)?.value as string,
+            description:(fields[1]?.field as SingleField)?.value as string
+        }
+        if(selectedItemId===""){
+            createSupplier(newSupplier)
+                .then(res => {
+                    setSupplierList(res.suppliers);
+                });
+        } else updateSupplier(newSupplier)
+            .then(res => {
+                setSupplierList(res.suppliers);
+            });
+        setShowAddEditComponent(false);
+    }
+    
+    const onAddSupplierClick = (e:React.MouseEvent<HTMLButtonElement>):void => {
+        const newFields:Field[] = [
+            {  
+                name:"Name",
+                display:"Name",
+                fieldType:"Single",
+                required: true,
+                field:{value:""}
+            },{
+                name: "Description",
+                display:"Description",
+                fieldType:"Single",
+                required:false,
+                field:{value:""}
+            }
+        ]
+        setAddEditTitle("Add Item");
+        setSaveButton(()=> onSaveSupplierButton);
+        setAddEditFields(newFields);
+    }
+
+    const onEditSupplierClick = (e:React.MouseEvent<HTMLButtonElement>):void => {
+        const supplierToEdit = supplierList.find(supplier => supplier._id === selectedSupplierId);
+        if(supplierToEdit === undefined) return;
+        const editFields:Field[] = [
+            {
+                name:"Name",
+                display:"Name",
+                fieldType:"Single",
+                required: true,
+                field:{value:supplierToEdit.name}
+            }, {
+                name: "Description",
+                display:"Description",
+                fieldType:"Single",
+                required:false,
+                field:{value:supplierToEdit.description}
+            }
+        ];
+        setAddEditTitle("Edit " + supplierToEdit.name);
+        setSaveButton(()=> onSaveSupplierButton);
+        setAddEditFields(editFields);
+    }
+    
+    const onDeleteSupplierClick = (e:React.MouseEvent<HTMLButtonElement>):void => {
+        const supplierToDelete = supplierList.find(supplier => supplier._id === selectedSupplierId);
+        if(supplierToDelete === undefined) return;
+        if(itemList.find(item => item.category === selectedCategoryId) !== undefined) {
+            setWarningMessage(text.warning.canNotDelete.replace("{replace}", supplierToDelete.name));
+            setCanCancel(false);
+            setShowWarning(true);
+            return;
+        }
+        setWarningMessage(text.warning.delete.replace("{replace}", supplierToDelete.name));
+        setWarningButton(()=> onConfirmDeleteSupplierClick);
+        setCanCancel(true);
+        setShowWarning(true);
+    }
+
+    const onConfirmDeleteSupplierClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+        setWarningButton(() => defaultButton);
+        deleteSupplier(selectedSupplierId).then(res => {
+            setSupplierList(res.suppliers);
+        }).catch(err => {
+            setWarningMessage(err);
+            setCanCancel(false);
+            setShowWarning(true);
+        });
+        setShowWarning(false);
     }
     
     const onCancelButton = (e:React.MouseEvent<HTMLButtonElement>):void => {
@@ -346,6 +494,40 @@ export const OrderManagement: React.FC<Props> = () => {
                 })}
                 currentId={selectedItemId}
                 setId={setSelectedItemId}
+            />
+            <h2>Suppliers</h2>
+            <div className={styles["buttonList"]}>
+                <ButtonComponent 
+                    name={<BsPlusLg />}
+                    onClick={onAddSupplierClick}
+                />
+                {selectedCategoryId !== ""?
+                    <>
+                        <ButtonComponent
+                            onClick={onEditSupplierClick}
+                            name={<BsPencil />}
+                            width={"100px"}
+                        />
+                        <ButtonComponent
+                            onClick={onDeleteSupplierClick}
+                            name={<BsTrash />}
+                            width={"100px"}
+                            isNegativeColor={true}
+                        />
+                    </>:
+                    ""
+                }
+            </div>
+            <DataTable
+                headers={["Name", "Description"]}
+                rows={supplierList.map(supplier => {
+                    return {
+                        id:supplier._id!,
+                        columns: [supplier.name, supplier.description||""]
+                    }
+                })}
+                currentId={selectedSupplierId}
+                setId={setSelectedSupplierId}
             />
         </>
     );
