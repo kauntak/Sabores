9008ae53b9e60e2461b70f7360ce8f31c5de1bef
kauntak <n.koshirae+github@gmail.com>
2022-09-01 22:04:28 -0600

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -24,7 +24,7 @@ mongoose.connect(MONGO_URI, (error):void => {
 });
 
 if(process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-    app.use(express.static("./../../client/build"));
+    // app.use(express.static("./../../client/build"));
     app.get('*', (req:Request, res:Response) => {
         res.sendFile(path.join(__dirname + './../../client/build/index.html'));
     })
